f90801fa5e2c2236b19506ab61a1124d606d30d6
blagasse <165404920+Blacg13@users.noreply.github.com>
2025-04-22 13:51:59 +0200

    feat: list of selected items

--- src/Components/Quizz/Guessbox.tsx
+++ src/Components/Quizz/Guessbox.tsx
@@ -6,48 +6,43 @@ interface GuessboxProps {
     dailyItem: string;
 }
 const Guessbox: React.FC<GuessboxProps> = ({ items, dailyItem }) => {
-  console.log("dailyItem: ", dailyItem);
-  
-    console.log("items: ", items);
+  const [filteredItems, setFilteredItems] = useState<string[]>([]);
+  const [selectedItems, setSelectedItems] = useState<string[]>([]);
+
+  function getItemsFilter(inputValue: string) {
+    const lowerCasedInputValue = inputValue.toLowerCase()
     
-    function getItemsFilter(inputValue: string) {
-        const lowerCasedInputValue = inputValue.toLowerCase()
-      
-        return function itemsFilter(items: string) {
-          return (
-            !inputValue ||
-            items.toLowerCase().includes(lowerCasedInputValue)
-          )
-        }
-      }
-    
-    const [filteredItems, setFilteredItems] = useState<string[]>([]);
-    
-    
-    const {
-        isOpen,
-        getToggleButtonProps,
-        getMenuProps,
-        getInputProps,
-        // highlightedIndex,
-        getItemProps,
-        // selectedItem,
+    return function itemsFilter(items: string) {
+      return (
+        !inputValue ||
+        items.toLowerCase().includes(lowerCasedInputValue)
+      )
+    }
+  }
+  const {
+    isOpen,
+    getToggleButtonProps,
+    getMenuProps,
+    getInputProps,
+    getItemProps,
       } = useCombobox({
         onInputValueChange({inputValue}) {
           setFilteredItems(items.filter(getItemsFilter(inputValue)))
         },
-        items,
+        items: filteredItems,
+        onSelectedItemChange({selectedItem}) {
+          if (selectedItem && !selectedItems.includes(selectedItem)) {
+            setSelectedItems(prev => [...prev, selectedItem]);
+          }
+        },
         itemToString(item) {
           return item ? item : ''
         },
-      })
-
+      })    
     return <section className={style.guessbox}>
-      {/* <div> */}
         <input placeholder="Devine le personnage !" {...getInputProps()} />
         <button {...getToggleButtonProps()} >{isOpen ? <>&#8593;</> : <>&#8595;</>}</button>
-      {/* </div> */}
-      <div>
+      <div className={style.optionsList}>
         <ul {...getMenuProps()}>
             {isOpen && filteredItems.map((item, index) => (
               <li key={item} {...getItemProps({item, index})}>
@@ -56,6 +51,13 @@ const Guessbox: React.FC<GuessboxProps> = ({ items, dailyItem }) => {
             ))}
         </ul>
       </div>
+      <section className={style.selectedList}>
+        <ul>
+            {selectedItems.map((item) => (
+              <li key={item} className={item === dailyItem ? style.correct : style.incorrect}>{item}</li>
+            ))}
+        </ul>
+      </section>
     </section>
 }
 export default Guessbox;
