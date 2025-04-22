b16c71aab23faf7c70d7c13fe920f61abbf12f8d
blagasse <165404920+Blacg13@users.noreply.github.com>
2025-04-22 11:22:07 +0200

    refactor: quote quiz

--- src/Components/Quizz/Guessbox.tsx
+++ src/Components/Quizz/Guessbox.tsx
@@ -53,71 +53,10 @@ const Guessbox: React.FC<GuessboxProps> = ({ items, dailyItem }) => {
             {isOpen && filteredItems.map((item, index) => (
               <li key={item} {...getItemProps({item, index})}>
                     {item}
-                </li>
+              </li>
             ))}
         </ul>
       </div>
     </section>
-
-
-
-    // const [inputValue, setInputValue] = useState<string>('');
-    // const [filteredItem, setFilteredItem] = useState<string>('');
-    // const [selectedItems, setSelectedItems] = useState<string[]>([]);
-    
-    // // const [error, setError] = useState<string>('');
-
-    // const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-    //     const input = e.currentTarget;
-    //     setInputValue(input.value);
-        
-    //     //? regex
-    //     // if (!/^[^0-9.]*$/.test(input.value)) {
-    //     //     setError('Tu ne peux écrire qu’avec des lettres !');
-    //     // } else {
-    //     //     setError('');
-    //     // }
-    //     // input.value = input.value.replace(/^[^0-9.]*$/g, '');
-
-    //     const filtered = items.filter((item) => item.toLowerCase().includes(input.value.toLowerCase()));
-    //     console.log("filtered: ", filtered);
-    //     setFilteredItem(filtered[0]);
-    //     console.log("filteredItem: ", filteredItem);
-    // }
-    // const preOptionsList = [...items.filter((option) => inputValue.includes(option))]
-    // const optionsList = [...preOptionsList.filter((option) => !selectedItems.includes(option))];
-    // const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
-    //     e.preventDefault();        
-    //     setSelectedItems([...new Set([...selectedItems, filteredItem])]);
-    //     console.log("selectedItems: ", selectedItems);
-        
-    // }
-    // return (
-    //     <>
-    //     <section className={style.guessbox}>
-    //         <form>
-    //             <input type="text" name="guessbox" list="itemsList" onInput={handleInput}/>
-    //                 {/* <datalist id='itemsList'>
-    //                     {items.filter((item) => !selectedItems.includes(item)).map((item) => (
-    //                         <option key={item} value={item} />
-    //                     ))}
-    //                 </datalist> */}
-    //             <button onClick={handleClick}>Valider</button>
-    //             <ul>
-    //                 {optionsList.map((option) => (
-    //                     <li key={option}>{option}</li>))}
-    //             </ul>
-    //             {/* {error && <p className={style.red}>{error}</p>} */}
-    //         </form>
-    //     </section>
-    //     <div>
-    //         <ul>
-    //         {selectedItems.map((item) => (
-    //             item ? <li className={dailyItem === item ? style.green : style.red} key={item}>{item}</li> : null
-    //         ))}
-    //         </ul>
-    //     </div>
-    //     </>
-    // )
 }
 export default Guessbox;
